c91eeacce62538b4f327053d8d543ed0de0e1006
GongFuBoy <719630857@qq.com>
2020-04-23 19:38:41 +0800

    字符串转整数

---
+++ src/main/java/com/github/gongfuboy/leetcode/meidium/Solution8.java
@@ -0,0 +1,20 @@
+package com.github.gongfuboy.leetcode.meidium;
+
+
+/**
+ * 字符串转整数
+ */
+public class Solution8 {
+
+    public int myAtoi(String str) {
+        char[] chars = str.toCharArray();
+        char startChar;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            startChar = chars[i];
+            if (startChar == ' ') continue;
+
+        }
+        return 0;
+    }
+}
