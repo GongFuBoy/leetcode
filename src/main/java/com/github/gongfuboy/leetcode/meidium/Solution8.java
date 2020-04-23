334a28b1a04e63648deb733a88700f7ccc24fc5c
GongFuBoy <719630857@qq.com>
2020-04-23 23:54:49 +0800

    字符串转整数

--- src/main/java/com/github/gongfuboy/leetcode/meidium/Solution8.java
+++ src/main/java/com/github/gongfuboy/leetcode/meidium/Solution8.java
@@ -7,14 +7,38 @@ package com.github.gongfuboy.leetcode.meidium;
 public class Solution8 {
 
     public int myAtoi(String str) {
-        char[] chars = str.toCharArray();
-        char startChar;
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < chars.length; i++) {
-            startChar = chars[i];
-            if (startChar == ' ') continue;
+        long tempResult = 0;
+        int result = 0;
 
+        // 首先处理str前后空格
+        String trim = str.trim();
+        // 判断异常情况
+        if (trim.length() == 0) return 0;
+        if (!Character.isDigit(trim.toCharArray()[0]) && trim.toCharArray()[0] != '-' && trim.toCharArray()[0] != '+') {
+            return 0;
         }
-        return 0;
+        boolean flag = trim.toCharArray()[0] == '-';
+        int index = Character.isDigit(trim.toCharArray()[0]) ? 0 : 1;
+
+        while (index < trim.length() && Character.isDigit(trim.toCharArray()[index])) {
+            int sourceInt = trim.toCharArray()[index] - '0';
+            tempResult = tempResult * 10 + sourceInt;
+            if (flag && tempResult > Integer.MAX_VALUE + 1L) {
+                return Integer.MIN_VALUE;
+            }
+            if (!flag && tempResult > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            index++;
+        }
+
+        if (flag) return (int) -tempResult;
+        else return (int) tempResult;
     }
+
+    public static void main(String[] args) {
+        Solution8 solution8 = new Solution8();
+        System.out.println(solution8.myAtoi("-42"));
+    }
+
 }
