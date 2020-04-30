dbadc6015343dd0015208ac0fba0ef29c2baa439
GongFuBoy <719630857@qq.com>
2020-04-30 21:53:48 +0800

    两数相除, 下一个排列

---
+++ src/main/java/com/github/gongfuboy/leetcode/meidium/Solution29.java
@@ -0,0 +1,85 @@
+package com.github.gongfuboy.leetcode.meidium;
+
+/**
+ * 两数相除
+ *
+ *
+ 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+
+ 返回被除数 dividend 除以除数 divisor 得到的商。
+
+ 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+
+
+
+ 示例 1:
+
+ 输入: dividend = 10, divisor = 3
+ 输出: 3
+ 解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+ 示例 2:
+
+ 输入: dividend = 7, divisor = -3
+ 输出: -2
+ 解释: 7/-3 = truncate(-2.33333..) = -2
+
+
+ 提示：
+
+ 被除数和除数均为 32 位有符号整数。
+ 除数不为 0。
+ 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。本题中，如果除法结果溢出，则返回 231 − 1。
+ */
+public class Solution29 {
+
+    public int divide(int dividend, int divisor) {
+        long result = 0;
+
+        if (dividend == 0) {
+            return 0;
+        }
+        if (dividend > 0 && divisor > 0) {
+            while (dividend >= divisor) {
+                dividend = dividend - divisor;
+                result++;
+            }
+        }
+        if (dividend > 0 && divisor < 0) {
+            while (dividend > 0) {
+                dividend = dividend + divisor;
+                result--;
+            }
+            if (dividend != 0) {
+                result++;
+            }
+        }
+
+        if (dividend < 0 && divisor > 0) {
+            while (dividend < 0) {
+                dividend = dividend + divisor;
+                result--;
+            }
+            if (dividend != 0) {
+                result++;
+            }
+        }
+
+        if (dividend < 0 && divisor < 0) {
+            while (dividend <= divisor) {
+                dividend = dividend - divisor;
+                result++;
+            }
+        }
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        return (int) result;
+    }
+
+    public static void main(String[] args) {
+        Solution29 solution29 = new Solution29();
+        System.out.println(solution29.divide(2147483647, 2));
+        System.out.println(2147483647 / 2);
+    }
+
+}
---
+++ src/main/java/com/github/gongfuboy/leetcode/meidium/Solution31.java
@@ -0,0 +1,56 @@
+package com.github.gongfuboy.leetcode.meidium;
+
+/**
+ * 下一个排列
+ *
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ *
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ *
+ * 必须原地修改，只允许使用额外常数空间。
+ *
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ *
+ */
+public class Solution31 {
+
+    public void nextPermutation(int[] nums) {
+
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        int start = i + 1;
+        int end = nums.length -1;
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.println("----");
+    }
+
+    public static void main(String[] args) {
+        Solution31 solution31 = new Solution31();
+        int[] ints = {1,3,2};
+        solution31.nextPermutation(ints);
+    }
+
+}
