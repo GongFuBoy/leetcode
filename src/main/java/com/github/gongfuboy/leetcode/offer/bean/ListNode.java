95a23615102c9adbd60bb3cb6b4858ec7811e613
GongFuBoy <719630857@qq.com>
2021-01-11 20:21:58 +0800

    add Offer18 添加链表节点bean

---
+++ src/main/java/com/github/gongfuboy/leetcode/offer/bean/ListNode.java
@@ -0,0 +1,15 @@
+package com.github.gongfuboy.leetcode.offer.bean;
+
+import com.github.gongfuboy.leetcode.offer.Offer18;
+
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+
+
+
+}
