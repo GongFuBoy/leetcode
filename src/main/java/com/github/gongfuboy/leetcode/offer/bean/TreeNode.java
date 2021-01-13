cfb9ceceaf625381f0b41f61539bc4c78fe341ab
GongFuBoy <719630857@qq.com>
2021-01-13 19:57:07 +0800

    add 添加TreeNode

---
+++ src/main/java/com/github/gongfuboy/leetcode/offer/bean/TreeNode.java
@@ -0,0 +1,10 @@
+package com.github.gongfuboy.leetcode.offer.bean;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
