61bf03b920de7ee91aa1f1f92da10a082646275d
GongFuBoy <719630857@qq.com>
2022-01-13 14:55:31 +0800

    add 链表逆置

--- src/main/java/com/github/gongfuboy/leetcode/offer/Offer06.java
+++ src/main/java/com/github/gongfuboy/leetcode/offer/Offer06.java
@@ -1,33 +1,34 @@
 package com.github.gongfuboy.leetcode.offer;
 
+import com.github.gongfuboy.leetcode.offer.bean.ListNode;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Offer06 {
 
     public int[] reversePrint(ListNode head) {
-        ArrayList<Integer> integers = new ArrayList<>();
-        addNodeToList(integers, head);
-        int[] result = new int[integers.size()];
-        for (int i = 0; i < integers.size(); i++) {
-            result[i] = integers.get(i);
+        if (head == null) {
+            return new int[0];
+        }
+
+        Stack<ListNode> listNodes = new Stack<>();
+        while (head != null) {
+            listNodes.add(head);
+            head = head.next;
+        }
+
+        int[] result = new int[listNodes.size()];
+        int index = 0;
+        while (!listNodes.isEmpty()) {
+            result[index] = listNodes.pop().val;
+            index++;
         }
         return result;
-    }
-
-    private void addNodeToList(List<Integer> result, ListNode head) {
-        if (head == null) return;
-        addNodeToList(result, head.next);
-        result.add(head.val);
-    }
-
-    private static final class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) {
-            val = x;
-        }
 
     }
+
+
 
 }
