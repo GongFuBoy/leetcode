e1e111d1a69d17fc264f0121f4a23bef1f241e58
GongFuBoy <719630857@qq.com>
2022-01-17 16:26:19 +0800

    add 最小的k个数

--- src/main/java/com/github/gongfuboy/leetcode/offer/Offer40.java
+++ src/main/java/com/github/gongfuboy/leetcode/offer/Offer40.java
@@ -3,7 +3,39 @@ package com.github.gongfuboy.leetcode.offer;
 public class Offer40 {
 
     public int[] getLeastNumbers(int[] arr, int k) {
-        return null;
+        quickSort(arr, 0, arr.length - 1);
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = arr[i];
+        }
+        return result;
+    }
+
+    private void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = findPivot(arr, left, right);
+            quickSort(arr, left, pivot - 1);
+            quickSort(arr, pivot + 1, right);
+        }
+    }
+
+    private int findPivot(int[] arr, int left, int right) {
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index ++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
 }
