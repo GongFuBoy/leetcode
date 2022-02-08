b11807bc49fae1092dbeecf3de186f8b054f1924
GongFuBoy <719630857@qq.com>
2022-02-08 18:01:29 +0800

    add 连续子数组的最大和

--- src/main/java/com/github/gongfuboy/leetcode/offer/Offer42.java
+++ src/main/java/com/github/gongfuboy/leetcode/offer/Offer42.java
@@ -3,15 +3,23 @@ package com.github.gongfuboy.leetcode.offer;
 public class Offer42 {
 
     public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length < 1) {
+        if (nums == null || nums.length == 0) {
             return -1;
         }
-        int result = nums[0];
+        int[] f = new int[nums.length];
+
+        // 初始状态
+        f[0] = nums[0];
+        int result = f[0];
+
+        // 状态转移过程
         for (int i = 1; i < nums.length; i++) {
-            nums[i] = nums[i] + Math.max(nums[i - 1], 0);
-            result = Math.max(nums[i], result);
+            f[i] = Math.max(f[i - 1] + nums[i], nums[i]);
+            result = Math.max(f[i], result);
         }
+
         return result;
+
     }
 
 }
