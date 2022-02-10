578608d72d1d760262d10346d655290feb6e4b7a
GongFuBoy <719630857@qq.com>
2022-02-10 17:40:32 +0800

    add 二叉搜索树的最近公共祖先

---
+++ src/main/java/com/github/gongfuboy/leetcode/offer/Offer68_1.java
@@ -0,0 +1,21 @@
+package com.github.gongfuboy.leetcode.offer;
+
+import com.github.gongfuboy.leetcode.offer.bean.TreeNode;
+
+public class Offer68_1 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+}
