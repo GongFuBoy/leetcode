9d7ff98049274e905a70cd7c7fd136edf1969aff
GongFuBoy <719630857@qq.com>
2022-02-11 18:05:15 +0800

    add 二维数组中的查找

--- src/main/java/com/github/gongfuboy/leetcode/offer/Offer04.java
+++ src/main/java/com/github/gongfuboy/leetcode/offer/Offer04.java
@@ -2,26 +2,31 @@ package com.github.gongfuboy.leetcode.offer;
 
 public class Offer04 {
 
+    private boolean[][] flagMatrix;
+
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        // 使用dfs进行遍历
         if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
             return false;
         }
-        int rows = matrix.length;
-        int lines = matrix[0].length;
-
-        int startRow = 0;
-        int startLine = lines - 1;
-        while (startRow < rows && startLine > -1) {
-            if (matrix[startRow][startLine] == target) {
-                return true;
-            } else if (matrix[startRow][startLine] > target) {
-                startLine--;
-            } else {
-                startRow++;
-            }
-
-        }
-        return false;
+        flagMatrix = new boolean[matrix.length][matrix[0].length];
+        return dfs(matrix, target, 0, 0);
     }
+
+    private boolean dfs(int[][] matrix, int target, int left, int right) {
+        // 判断是否被访问过
+        if (left >= matrix.length || right >= matrix[0].length) {
+            return false;
+        }
+        if (flagMatrix[left][right]) {
+            return false;
+        }
+        flagMatrix[left][right] = true;
+        if (matrix[left][right] == target) {
+            return true;
+        }
+        return dfs(matrix, target, left + 1, right) || dfs(matrix, target, left, right + 1);
+    }
+
 
 }
